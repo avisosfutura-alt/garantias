fe3d6e5009c1790e9524cf9e7a841f3a61d3bf15
avisosfutura-alt <avisosfutura@gmail.com>
2026-08-11 11:55:11 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
  * Eso obliga a todos los equipos a descargar el archivo nuevo.
  ******************************************************************************/
 
-var VERSION = 'garantias-v33';
+var VERSION = 'garantias-v34';
 
 var CASCARON = [
   './',
